2a41986423764990e6f6ac9fb8ae6461551c9795
shyamkrishna <rskshyam@gmail.com>
2022-11-24 09:13:42 +0530

    sanjana 1st collab with shyam

--- example1.py
+++ example1.py
@@ -5,3 +5,7 @@ b = 20
 # Now adding new varibale f for 3rd commit from example1
 
 f = 1000
+
+# adding below line from sanjana account to collabarte with shyam
+
+Print ("Hi, i am very glad to work with you")
