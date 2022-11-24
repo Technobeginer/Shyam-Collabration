a303ecdba9fea275795f4d1c1265a4b4fe32bead
shyamkrishna <rskshyam@gmail.com>
2022-11-24 10:19:24 +0530

    rues1 commit

--- example1.py
+++ example1.py
@@ -9,3 +9,7 @@ f = 1000
 # adding below line from sanjana account to collabarte with shyam
 
 Print ("Hi, i am very glad to work with you")
+
+# after adding rules to user 1 trying one mor print
+
+hi, hello
