d8cfd4cdb733fdb18a6fd3d6547da957435c0cdf
shyamkrishna <rskshyam@gmail.com>
2022-11-24 09:15:22 +0530

    2nd commit for corodination from sanju to shyam

--- example2.py
+++ example2.py
@@ -5,3 +5,7 @@ d = 40
 # Now adding new varibale e for 2nd commit from example2 
 
 e = 100
+
+# adding below line from sanjana account to collabarte with shyam for 2nd commit
+
+Print ("it was so pleasure to coordinate with you again")
